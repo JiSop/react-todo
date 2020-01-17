cb7f44741aa593366799625835ea681867ee74cf
JiSop <ddie0821@gmail.com>
2020-01-17 16:31:38 +0900

    Apply redux-actions

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,50 +1,32 @@
+import { createAction, handleActions } from 'redux-actions';
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
 const ACTIVE_TAB = 'todos/ACTIVE_TAB';
 const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
 const TOGGLE_ALL = 'todos/TOGGLE_ALL';
-const REMOVE_COMPLETED =  'todos/REMOVE_COMPLETED';
+const REMOVE_COMPLETED = 'todos/REMOVE_COMPLETED';
 
 let id = 1;
 
-export const insert = content => ({
-  type: INSERT,
-  todo: {
-    id: id++,
-    content,
-    completed: false
-  }
-});
+export const insert = createAction( INSERT, content => ( {
+  id: id++,
+  content,
+  completed: false
+} ) );
 
-export const changeInput = input => ({
-  type: CHANGE_INPUT,
-  input
-});
+export const changeInput = createAction( CHANGE_INPUT, input => input );
 
-export const toggle = id => ({
-  type: TOGGLE,
-  id
-});
+export const toggle = createAction( TOGGLE, id => id );
 
-export const remove = id => ({
-  type: REMOVE,
-  id
-});
+export const remove = createAction( REMOVE, id => id );
 
-export const toggleAll = checked => ({
-  type: TOGGLE_ALL,
-  checked
-});
+export const toggleAll = createAction( TOGGLE_ALL, checked => checked );
 
-export const removeCompleted = () => ({
-  type: REMOVE_COMPLETED,
-});
+export const removeCompleted = createAction( REMOVE_COMPLETED );
 
-export const activeTab = tab => ({
-  type: ACTIVE_TAB,
-  tab
-});
+export const activeTab = createAction( ACTIVE_TAB, tab => tab );
 
 const initialState = {
   tabState: 'All',
@@ -52,46 +34,35 @@ const initialState = {
   todos: []
 };
 
-function todos( state = initialState, action ) {
-  switch ( action.type ) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos: state.todos.concat( action.todo )
-      };
-    case TOGGLE:
-      return {
-        ...state,
-        todos: state.todos.map( todo => todo.id === action.id ? {...todo, completed: !todo.completed} : todo )
-      };
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter( todo => todo.id !== action.id )
-      };
-    case TOGGLE_ALL:
-      return {
-        ...state,
-        todos: state.todos.map( todo => ( { ...todo, completed: action.checked } ) )
-      };
-    case REMOVE_COMPLETED:
-      return {
-        ...state,
-        todos: state.todos.filter( todo => todo.completed === false )
-      };
-    case ACTIVE_TAB:
-      return {
-        ...state,
-        tabState: action.tab
-      };
-    default:
-      return state;
-  }
-}
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: ( state, { payload: input } ) => ( { ...state, input } ),
+    [INSERT]: ( state, { payload: todo } ) => ( {
+      ...state,
+      todos: state.todos.concat( todo ),
+    } ),
+    [TOGGLE]: ( state, { payload: id }) => ( {
+      ...state,
+      todos: state.todos.map( todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo ),
+    }),
+    [REMOVE]: ( state, { payload: id} ) => ( {
+      ...state,
+      todos: state.todos.filter( todo => todo.id !== id ),
+    }),
+    [TOGGLE_ALL]: ( state, { payload: checked }) => ({
+      ...state,
+      todos: state.todos.map( todo => ( { ...todo, completed: checked } ) ),
+    }),
+    [REMOVE_COMPLETED]: ( state ) => ( {
+      ...state,
+      todos: state.todos.filter( todo => todo.completed === false ),
+    }),
+    [ACTIVE_TAB]: ( state, { payload: tab }) => ( {
+      ...state,
+      tabState: tab
+    }),
+  },
+  initialState,
+);
 
 export default todos;
